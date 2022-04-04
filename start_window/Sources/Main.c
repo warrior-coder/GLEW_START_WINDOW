4f80c0569183fba6a2b9e741b7fd33a3d5ecf50c
warrior-coder <gudkou2003@gmail.com>
2022-04-04 10:02:19 +0300

    RGB triangle.

--- start_window/Sources/Main.c
+++ start_window/Sources/Main.c
@@ -3,40 +3,56 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
+    // initialize the library
     if (!glfwInit())
     {
         return 1;
     }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(400, 400, "GLFW Start Window", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // make the window's context current
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK)
     {
-        return  1;
+        glfwTerminate();
+        return 1;
     }
 
-    /* Loop until the user closes the window */
+    // loop until the user closes the window
+    float angle = 0;
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // render
+        glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        glPushMatrix();
+
+        glRotatef(angle, 0, 0, 1);
+        angle += 0.1F;
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0); glVertex2f(0, 1);
+        glColor3f(0, 1, 0); glVertex2f(0.87F, -0.5F);
+        glColor3f(0, 0, 1); glVertex2f(-0.87F, -0.5F);
+        glEnd();
+
+        glPopMatrix();
+
+        // swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // poll for and process events
         glfwPollEvents();
     }
 
